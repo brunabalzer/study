71351013d7e93f62ddb27bb654496dc58a37bac7
bruna balzer <brunabalzer@alunos.utfpr.edu.br>
2024-03-30 13:41:10 -0300

    Add project files.

---
+++ inicio c sharp/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace inicio_c_sharp
+{
+    class Class1
+    {
+        string firstName;
+        List<double> numeros;
+
+        public Class1()
+        {
+            firstName = "Bob";
+            numeros = new List<double>() { 3, 34.4 };
+        }
+
+        public void DisplayMessage()
+        {
+            Console.Write($"Hello, {firstName}! You have {numeros[0]} messages in your inbox. The temperature is {numeros[1]} celsius.");
+        }
+
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.DisplayMessage();
+            Console.ReadLine();
+        }
+    }
+
+    class Class2
+    {
+
+
+
+    }
+}
---
+++ inicio c sharp/Program.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+
+//namespace iniciocsharp
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int minha = 1;
+//            Console.WriteLine(minha);
+//            Console.WriteLine(minha.GetType());
+//            Program2.MetodoExemplo();
+//            Console.ReadLine();
+//        }
+//    }
+
+//    class Program2
+//    {
+//        public static void MetodoExemplo()
+//        {
+//            char sexo = 'f';
+//            if (sexo == 'f')
+//            {
+//                int idade = 45;
+//                int cintura = 20;
+//                string nasc = "Brasileira";
+//                Console.WriteLine($"Idade: {idade}, Cintura: {cintura}, Nacionalidade: {nasc}");
+
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
